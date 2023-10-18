8ef5d6a00409267bdb7443cfb625ba1d91ce06cf
Donald Kibet <chelashawdonald@yahoo.com>
2023-10-18 23:54:55 +0300

    (feat) add HIV&CD4 trends panel

---
+++ packages/esm-patient-hiv-art-app/src/dashboard.meta.ts
@@ -0,0 +1,6 @@
+export const dashboardMeta = {
+  slot: 'patient-chart-hiv-art-treatment-dashboard-slot',
+  columns: 1,
+  path: 'HIV & ART Treatment',
+  title: 'HIV & ART Treatment',
+};
---
+++ packages/esm-patient-hiv-art-app/src/hooks/usePatientObs.tsx
@@ -0,0 +1,36 @@
+import { FHIRResource, fhirBaseUrl, openmrsFetch } from '@openmrs/esm-framework';
+import useSWR from 'swr';
+
+interface Obs {
+  entry: Array<FHIRResource>;
+  id: string;
+  resourceType: string;
+  total: number;
+  type: string;
+}
+
+/**
+ * Custom hook to fetch patient observations based on given patient UUID and concept UUIDs.
+ *
+ * @param {string} patientUuid - The UUID of the patient.
+ * @param {Array<string>} conceptUuids - An array of concept UUIDs.
+ * @returns {object} - An object containing the observed results, error, loading status, and validation status.
+ */
+export const usePatientObs = (patientUuid: string, conceptUuids: Array<string>) => {
+  const observationEndpoint = `${fhirBaseUrl}/Observation?subject:Patient=${patientUuid}&code=${conceptUuids.join(
+    ',',
+  )}`;
+
+  const { data, error, isLoading, isValidating } = useSWR<{ data: Obs }>(observationEndpoint, openmrsFetch);
+
+  const observedResults =
+    data?.data?.entry?.reduce((results, entry) => {
+      const resource = entry.resource;
+      if (typeof resource.valueQuantity?.value === 'number') {
+        results.push(resource);
+      }
+      return results;
+    }, []) ?? [];
+
+  return { obs: observedResults, error, isLoading, isValidating };
+};
