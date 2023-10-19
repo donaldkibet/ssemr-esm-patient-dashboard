dd960c7524eecb2cd41a9320bd6b1cc6c938140c
Donald Kibet <chelashawdonald@yahoo.com>
2023-10-19 11:24:09 +0300

    (feat) Add Client enrollment (#2)

--- packages/esm-patient-hiv-art-app/src/dashboard/hiv-art-dashboard.component.tsx
+++ packages/esm-patient-hiv-art-app/src/dashboard/hiv-art-dashboard.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
 import ViralLoadCD4Trend from './tab-panels/viral-load-cd4-trend.component';
+import ClientEnrollment from './tab-panels/client-enrollment.component';
 
 interface HIVArtDashboardProps {
   patientUuid: string;
@@ -13,10 +14,14 @@ const HIVArtDashboard: React.FC<HIVArtDashboardProps> = ({ patientUuid }) => {
     <Tabs>
       <TabList aria-label="List of tabs" contained>
         <Tab>{t('viralLoadAndCDTrend', 'Viral load and CD4 trends')}</Tab>
+        <Tab>{t('clientEnrollment', 'Client enrollment')}</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
           <ViralLoadCD4Trend patientUuid={patientUuid} />
+        </TabPanel>
+        <TabPanel>
+          <ClientEnrollment patientUuid={patientUuid} />
         </TabPanel>
       </TabPanels>
     </Tabs>
